c02380629d3b141221b8b3051191c0321c935d6d
Soo-Beom <soobeomkim77@gmail.com>
2021-03-02 10:23:02 -0800

    commit tree 0b9dc043bfcf01baeaeeb05674ee406344334582

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your code here
+// fetch("http://localhost:3000/dogs", configurationObject);
+    // configurationObject = {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+//   };
+
+// let configobject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+// body: JSON.stringify(userInfo)
+// }
+
+
+
+
+function submitData(name, email) {
+    let userInfo = {name: name, email: email}
+
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    body: JSON.stringify(userInfo)
+    };
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        let bodyEl = document.querySelector("body")
+        let p = document.createElement("p")
+        p.textContent = object.id
+        bodyEl.appendChild(p)
+    })
+    .catch(function(error){
+        alert("Bad things! Failed Post")
+        let bodyEl = document.querySelector("body")
+        let h3 = document.createElement("h3")
+        h3.textContent = error.message
+        bodyEl.appendChild(h3)
+    })
+}
